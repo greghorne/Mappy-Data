3b8aad971fbc9e14fffc05f8617e49aae212fcd1
greghorne <greghorne@hotmail.com>
2018-12-05 22:35:24 +0000

    debugged issue with check_region function call

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'map#index'
-  get  'check_region' => 'map#check_region'
+  post  'check_region' => 'map#check_region'
   post 'create_isochrone' => 'map#create_isochrone'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
